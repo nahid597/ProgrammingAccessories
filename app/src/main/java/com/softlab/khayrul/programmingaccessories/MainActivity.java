c9daa6ed5c618c7cf30e9ae96034a00150b2496b
KhAyRuL01 <khairulf16b@gmail.com>
2017-12-01 20:47:00 +0600

    trying to setup searching facilities

--- app/src/main/java/com/softlab/khayrul/programmingaccessories/MainActivity.java
+++ app/src/main/java/com/softlab/khayrul/programmingaccessories/MainActivity.java
@@ -7,13 +7,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
+    String string[] = new String[100];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+        try
+        {
+            InputStream file = getAssets().open("input.txt");
+            Scanner in = new Scanner(file);
+            int k = 0;
+            while(in.hasNext())
+            {
+                string[k] = null;
+                string[k++] = in.nextLine();
+            }
+
+        }catch(Exception e)
+        {
+
+        }
     }
+
     TextView txt;
     public void showBtn1(View v)
     {
@@ -45,16 +67,56 @@ public class MainActivity extends AppCompatActivity {
     {
         setContentView(R.layout.show);
         TextView txt = (TextView)findViewById(R.id.showtext);
+        String str = "nothing";
+        try
+        {
+            InputStream file = getAssets().open("input.txt");
+            Scanner in = new Scanner(file);
+           str = in.nextLine();
+
+        }catch(Exception e)
+        {
+
+        }
         Button button = (Button)v;
         String ss = button.getText().toString();
-        txt.setText(ss);
+        txt.setText(str);
     }
 
-    public void search()
+    public void search(View v)
     {
         EditText edit = (EditText)findViewById(R.id.editText);
-        StringBuilder string = new StringBuilder(edit.getText().toString());
-
+        StringBuilder str = new StringBuilder(edit.getText().toString());
+        while(str.indexOf(" ")!=-1)
+        {
+            str.deleteCharAt(str.indexOf(" "));
+        }
+        String search = new String(str);
+        int i, len = search.length();
+        search = search.toLowerCase();
+        boolean flag = false;
+        for(i = 0; i <32; i++)
+        {
+            String s = string[i].substring(0,len);
+            if(s.equals(search))
+            {
+                flag = true;
+                break;
+            }
+        }
+        if(flag)
+        {
+            int resID = getResources().getIdentifier(string[i], "id", getPackageName());
+            Button button = (Button)findViewById(resID);
+            button.setX(5);
+            button.setY(30);
+        }
+        else
+        {
+            setContentView(R.layout.show);
+            TextView txt = (TextView)findViewById(R.id.showtext);
+            txt.setText("Not found");
+        }
     }
 
     public void exitApp(View v)
